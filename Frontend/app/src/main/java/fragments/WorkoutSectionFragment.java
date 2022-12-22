006c06c5163aceed2c575e0acd68a656bb5677fd
Sam <sgm@iastate.edu>
2022-12-22 12:51:14 -0600

    WW12: Displays multiple blocks of routines for the user

--- Frontend/app/src/main/java/fragments/WorkoutSectionFragment.java
+++ Frontend/app/src/main/java/fragments/WorkoutSectionFragment.java
@@ -2,16 +2,53 @@ package fragments;
 
 import android.example.weightworks.R;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+
+import adapters.RoutinesAdapter;
+import objects.Routine;
 
 public class WorkoutSectionFragment extends Fragment {
+
+    ArrayList<Routine> routines;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
         return inflater.inflate(R.layout.fragment_workout_layout, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        initializeRoutines();
+        RecyclerView rvRoutines = (RecyclerView) view.findViewById(R.id.rvRoutines);
+
+        RoutinesAdapter routinesAdapter = new RoutinesAdapter(routines);
+        rvRoutines.setAdapter(routinesAdapter);
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        rvRoutines.setLayoutManager(gridLayoutManager);
+    }
+
+    private void initializeRoutines() {
+        routines = new ArrayList<>();
+        Routine routine1 = new Routine("Push Day", "1/1/22");
+        Routine routine2 = new Routine("Pull Day", "2/2/22");
+        Routine routine3 = new Routine("Leg Day", "3/3/22");
+        routines.add(routine1);
+        routines.add(routine2);
+        routines.add(routine3);
     }
 }
---
+++ Frontend/app/src/main/java/objects/Routine.java
@@ -0,0 +1,32 @@
+package objects;
+
+public class Routine {
+
+    private String name;
+
+    private String date;
+
+    public Routine() {
+    }
+
+    public Routine(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
