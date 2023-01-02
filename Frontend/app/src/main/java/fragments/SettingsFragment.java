b90fd545b7ced4cdb6bdc5c664f584fde1cbc214
Sam <sgm@iastate.edu>
2023-01-02 12:24:34 -0600

    WW20: basic settings dialog window created

---
+++ Frontend/app/src/main/java/fragments/SettingsFragment.java
@@ -0,0 +1,31 @@
+package fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.example.weightworks.R;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class SettingsFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.fragment_settings, (ViewGroup) getView(), false));
+        return builder.create();
+    }
+}
