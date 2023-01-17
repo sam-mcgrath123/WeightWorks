721d176858cfa18d39c7c761f20ca1960e14585b
Sam <sgm@iastate.edu>
2023-01-17 10:06:40 -0600

    WW16: values gets populated from secondary dialog

--- Frontend/app/src/main/java/fragments/workout/AddExerciseFragment.java
+++ Frontend/app/src/main/java/fragments/workout/AddExerciseFragment.java
@@ -7,27 +7,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.DialogFragment;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class AddExerciseFragment extends DialogFragment {
 
-    private String selectedPart;
-    private ArrayList<Integer> partList = new ArrayList<>();
-    private ArrayList<String> partArrayList = new ArrayList<>();
-    private String[] bodyParts = new String[]{"Chest", "Arms", "Legs"};
-    ArrayList<String> selectedBodyParts = new ArrayList<>();
+    //TODO add constants for secondDialogOptions
+
+    private String selectedOption;
+    private HashMap<Integer, String[]> secondDialogOptions = new HashMap<>();
+    View mainDialog;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        secondDialogOptions.put(0, new String[]{"Chest", "Arms", "Legs"});
+        secondDialogOptions.put(1, new String[]{"Barbell", "Dumbbell", "Machine/Other"});
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         builder = initializeAlertBuilder(builder, inflater);
@@ -49,60 +51,55 @@ public class AddExerciseFragment extends DialogFragment {
                         Objects.requireNonNull(AddExerciseFragment.this.getDialog()).cancel();
                     }
                 });
-        View view = inflater.inflate(R.layout.fragment_add_exercise, null, false);
-        EditText editText = view.findViewById(R.id.add_exercise_name_text);
-        editText.setHint("WOW");
+        mainDialog = inflater.inflate(R.layout.fragment_add_exercise, null, false);
 
-        CardView cardView = view.findViewById(R.id.add_exercise_popup_body_part);
-        cardView.setOnClickListener(new View.OnClickListener() {
+        CardView bodyPartCard = mainDialog.findViewById(R.id.add_exercise_popup_body_part);
+        bodyPartCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
-                builder.setTitle("Body Part");
-                builder.setCancelable(true);
-                selectedPart = "";
-
-                builder.setSingleChoiceItems(bodyParts, 1, new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        selectedPart = bodyParts[which].toString();
-                    }
-                });
-
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        StringBuilder stringBuilder = new StringBuilder();
-                        for (int j = 0; j < partList.size(); j++) {
-                            stringBuilder.append(bodyParts[partList.get(j)]);
-                            selectedBodyParts.add(bodyParts[partList.get(j)]);
-                            if (j != partList.size() - 1) {
-                                stringBuilder.append(", ");
-                            }
-                        }
-                    }
-                });
-
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-//                builder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        for (int j = 0; j < selectedParts.length; j++) {
-//                            selectedParts[j] = false;
-//                            partList.clear();
-//                        }
-//                    }
-//                });
-                builder.show();
+                showSecondDialog(bodyPartCard, 0);
             }
         });
-        builder.setView(view);
+        CardView categoryCard = mainDialog.findViewById(R.id.add_exercise_popup_category);
+        categoryCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSecondDialog(categoryCard, 1);
+            }
+        });
+        builder.setView(mainDialog);
         return builder;
+    }
+
+    private void showSecondDialog(CardView secondaryDialogView, int optionsKey) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        String title = optionsKey == 0 ? "Body Part" : "Category";
+        builder.setTitle(title);
+        builder.setCancelable(true);
+        selectedOption = "";
+
+        builder.setSingleChoiceItems(secondDialogOptions.get(optionsKey), -1, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedOption = secondDialogOptions.get(optionsKey)[which].toString();
+            }
+        });
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                TextView value = secondaryDialogView.findViewById(R.id.add_exercise_input);
+                value.setText(selectedOption);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
