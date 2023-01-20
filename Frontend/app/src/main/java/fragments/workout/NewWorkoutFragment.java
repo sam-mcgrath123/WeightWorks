cc901ec3939ac833594d3afa12f0b62a88eb6357
Sam <sgm@iastate.edu>
2023-01-20 11:44:09 -0600

    WW28, both buttons showing but need to change to only show at the end of page

--- Frontend/app/src/main/java/fragments/workout/NewWorkoutFragment.java
+++ Frontend/app/src/main/java/fragments/workout/NewWorkoutFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
