225cdb584c9d9f74b31295f1f25388e89e1e7fd3
Sam <sgm@iastate.edu>
2023-01-21 12:25:41 -0600

    WW26: set up backend

---
+++ Backend/src/test/java/com/weightworks/commons/WeightWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weightworks.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
