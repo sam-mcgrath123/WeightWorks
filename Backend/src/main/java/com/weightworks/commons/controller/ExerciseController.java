91c209437528db333662ede596d3568c45559570
Sam <sgm@iastate.edu>
2023-01-23 15:49:47 -0600

    WW40: added set mapping

--- Backend/src/main/java/com/weightworks/commons/controller/ExerciseController.java
+++ Backend/src/main/java/com/weightworks/commons/controller/ExerciseController.java
@@ -2,6 +2,7 @@ package com.weightworks.commons.controller;
 
 import com.weightworks.commons.dao.ExerciseDao;
 import com.weightworks.commons.entity.Exercise;
+import com.weightworks.commons.entity.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,14 @@ public class ExerciseController {
     @Autowired
     ExerciseDao exerciseDao;
 
+    @PostMapping
+    public ResponseEntity<Exercise> create(@RequestBody Exercise exercise) {
+        Exercise savedExercise = exerciseDao.save(exercise);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedExercise.getId()).toUri();
+        return ResponseEntity.created(location).body(savedExercise);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Exercise> getById(@PathVariable Integer id) {
         Optional<Exercise> optionalExercise = exerciseDao.findById(id);
@@ -25,11 +34,12 @@ public class ExerciseController {
         );
     }
 
-    @PostMapping
-    public ResponseEntity<Exercise> create(@RequestBody Exercise exercise) {
-        Exercise savedExercise = exerciseDao.save(exercise);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(savedExercise.getId()).toUri();
-        return ResponseEntity.created(location).body(savedExercise);
+    @GetMapping("/{id}/sets")
+    public ResponseEntity<java.util.Set<Set>> getAllSets(@PathVariable Integer id) {
+        Optional<Exercise> optionalExercise = exerciseDao.findById(id);
+        if(optionalExercise.isEmpty()) {
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        return ResponseEntity.ok(optionalExercise.get().getSets());
     }
 }
