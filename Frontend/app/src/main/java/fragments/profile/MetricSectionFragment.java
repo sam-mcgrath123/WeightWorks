fab69a1be77b188badabb86f9e791d1958914371
Sam <sgm@iastate.edu>
2023-01-30 13:38:25 -0600

    I like progress circle

--- Frontend/app/src/main/java/fragments/profile/MetricSectionFragment.java
+++ Frontend/app/src/main/java/fragments/profile/MetricSectionFragment.java
@@ -1,65 +1,47 @@
 package fragments.profile;
 
+import android.example.weightworks.R;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.example.weightworks.R;
+import android.widget.ProgressBar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MetricSectionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 public class MetricSectionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MetricSectionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MetricSectionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MetricSectionFragment newInstance(String param1, String param2) {
-        MetricSectionFragment fragment = new MetricSectionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private int currentProgress = 0;
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_metric_section, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        progressBar = view.findViewById(R.id.metric_progress_bar);
+        progressBar.setProgress(currentProgress);
+
+        CountDownTimer countDownTimer = new CountDownTimer(10000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                currentProgress = currentProgress + 10;
+                progressBar.setProgress(currentProgress);
+                progressBar.setMax(100);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+        };
+
+        countDownTimer.start();
     }
 }
