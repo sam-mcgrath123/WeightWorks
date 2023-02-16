7a1dcec20327feea088a9d0c472cdf86b2c45d32
Sam <sgm@iastate.edu>
2023-02-16 16:41:11 -0600

    WW44: created DAO

---
+++ Backend/src/main/java/com/weightworks/commons/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.weightworks.commons.dao;
+
+import com.weightworks.commons.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
+}
