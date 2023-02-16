7e7a9404e66550283f8a20375465f01dfbbc0bb6
Sam <sgm@iastate.edu>
2023-02-16 16:34:44 -0600

    WW44: entity created

---
+++ Backend/src/main/java/com/weightworks/commons/entity/User.java
@@ -0,0 +1,75 @@
+package com.weightworks.commons.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "number_of_workouts")
+    private int numberOfWorkouts;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getNumberOfWorkouts() {
+        return numberOfWorkouts;
+    }
+
+    public void setNumberOfWorkouts(int numberOfWorkouts) {
+        this.numberOfWorkouts = numberOfWorkouts;
+    }
+}
